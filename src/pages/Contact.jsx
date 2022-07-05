12a3870689fce2898d38e1d3add634a92b14c59a
Nawel Benaicha <nawelbenaicha@MacBook-Pro-de-Nawel.local>
2022-07-05 21:28:47 +0200

    contact for mobile

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -16,18 +16,29 @@ const Contact = () => {
         <h1 className="text-center text-5xl mt-5 mb-5"> CONTACT </h1>
       </div>
       <ContactForm />
-      <div className="m:hidden">
-        <a
-          href="mailto: nawelbenaicha@gmail.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <h1>mail</h1>
-        </a>
-        <h2>linkedin</h2>
-        <h3>CV</h3>
-        <Footer />
+      <div className="flex flex-col justify-center">
+        <img className="-mt-10" src="../assets/formcat.png" />
+        <div className="m:hidden font-body text-2xl tracking-widest h-52 justify-around text-blue flex flex-col items-center">
+          <a
+            href="mailto: nawelbenaicha@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <h1 className="cursor-pointer hover:text-peche"> Mail</h1>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/nawelbch/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <h2 className="cursor-pointer hover:text-peche">Linkedin</h2>
+          </a>
+          <a href="https://github.com/nahabee" target="_blank" rel="noreferrer">
+            <h3 className="cursor-pointer hover:text-peche">GitHub</h3>
+          </a>
+        </div>{" "}
       </div>
+      <Footer />
     </div>
   );
 };
