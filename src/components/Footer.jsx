521e9460a10e0382bdb2e40c22d8bf73af011fa3
Nawel Benaicha <nawelbenaicha@MBP-de-Nawel.lan>
2022-07-05 14:20:12 +0200

    form button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-scroll";
 
 const Footer = () => {
   return (
-    <div className="flex font-body text-blue  p-5 items-end justify-end ">
+    <div className="flex font-body text-blue  p-5 items-end justify-end m:hidden ">
       <Link smooth="true" duration="700" to="/">
         <h1 className="hover:text-peche hover:tracking-widest cursor-pointer">
           {" "}
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,6 @@
 import ContactForm from "../components/ContactForm";
 import Footer from "../components/Footer";
 
-import { Link } from "react-scroll";
-
 const Contact = () => {
   return (
     <div
@@ -17,8 +15,17 @@ const Contact = () => {
       <div className=" text-blue flex flex-col items-center font-mtitle m:hidden">
         <h1 className="text-center text-5xl mt-5 mb-5"> CONTACT </h1>
       </div>
-      <div className="">
-        <ContactForm />
+      <ContactForm />
+      <div className="m:hidden">
+        <a
+          href="mailto: nawelbenaicha@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <h1>mail</h1>
+        </a>
+        <h2>linkedin</h2>
+        <h3>CV</h3>
         <Footer />
       </div>
     </div>
