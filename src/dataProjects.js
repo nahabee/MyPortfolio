41a304f92783ea05b7ac418f49289dd052113368
Nawël <95285146+nahabee@users.noreply.github.com>
2023-07-05 21:26:20 +0200

    new

--- src/dataProjects.js
+++ src/dataProjects.js
@@ -1,11 +1,11 @@
 const dataProjects = [
   {
     id: 1,
-    name: "Klar",
-    img: "../assets/klar.png",
-    url: "https://klar-underwear.vercel.app/",
-    title: " UNDERWEAR NATURAL BRAND. ",
-    desc: "Klar is a brand that simply lets you be you! To be who you are every day and every night. Sensual, natural, modest, sophisticated, wise and mysterious… What does it matter!",
+    name: "Agibask",
+    img: "../assets/agibask.png",
+    url: "https://agibask.vercel.app",
+    title: "THE GREEN PLATEFORM.",
+    desc: "A platform for connecting individuals and associations for ecological and collective purposes in the Basque Country. With a simple and effective interface in order to carry out more concrete and sustainable actions in order to reduce the impact of pollution and make the general public aware of the safeguarding of our territory.",
     techno: "React, Tailwind",
   },
   {
@@ -17,15 +17,7 @@ const dataProjects = [
     desc: "Xpanse agency has gained worldwide attention for a series of historic milestones. It is the only private company capable of returning a spacecraft from low-Earth orbit, and in 2012 our Dragon spacecraft became the first commercial spacecraft to deliver cargo to and from the International Space Station.",
     techno: "React, API rest, Tailwind",
   },
-  {
-    id: 3,
-    name: "Agibask",
-    img: "../assets/agibask.png",
-    url: "https://agibask.vercel.app",
-    title: "THE GREEN PLATEFORM.",
-    desc: "A platform for connecting individuals and associations for ecological and collective purposes in the Basque Country. With a simple and effective interface in order to carry out more concrete and sustainable actions in order to reduce the impact of pollution and make the general public aware of the safeguarding of our territory.",
-    techno: "React, Tailwind",
-  },
+
   {
     id: 4,
     name: "Upside",
@@ -45,7 +37,15 @@ const dataProjects = [
     desc: "The natural and peaceful atmosphere of the restaurant is aligned with this environment between land and sea. We were also exceptionally lucky that the architecture and design of the place was carried out by India Mahdavi. The restaurant also has a terrace outdoor area ideal for sunny days, as well as a vegetated winter garden, echoing the land-sea cuisine of our chef.",
     techno: "Html, Sass",
   },
-
+  {
+    id: 1,
+    name: "Bnch Creator",
+    img: "../assets/bnch.png",
+    url: "https://www.bnch-creator.com/",
+    title: "THE CONTENT CREATOR",
+    desc: "The website of a digital creator in full development. Between web design and UGC creation, it stands out for the quality of its unique digital content. This site allows you to learn more about her artistic touch and the offer she offers you.",
+    techno: "React, NextJS, Tailwind",
+  },
   {
     id: 5,
     name: "Brille",
@@ -54,6 +54,15 @@ const dataProjects = [
     title: "BRILLE, VEGAN HANDBAGS.",
     desc: "Brille is an eco-responsible and vegan brand of handbags made in Spain in a workshop on a human scale, in the heart of the Ubrique region. Brille then offers a qualitative product above all, durable, ethical, good for the planet, chic, glamorous and in bright colors, to shine in all circumstances, wherever you are.",
     techno: "Typescript, ExpressJS, MySQL",
+  },
+  {
+    id: 6,
+    name: "Klar",
+    img: "../assets/klar.png",
+    url: "https://klar-underwear.vercel.app/",
+    title: " UNDERWEAR NATURAL BRAND. ",
+    desc: "Klar is a brand that simply lets you be you! To be who you are every day and every night. Sensual, natural, modest, sophisticated, wise and mysterious… What does it matter!",
+    techno: "React, Tailwind",
   },
 ];
 
